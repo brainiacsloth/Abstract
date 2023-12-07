37bf9cbf34fedb6029f2409cd6c53cc549e2c39f
A253562 <A253562@LSBSA-9JJCSV3>
2023-12-07 02:25:04 +0200

    Add project files.

---
+++ Class2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract
+{
+    abstract class Vehicle
+
+    {
+        public int wheels = 4;
+
+    }
+
+    class Corsa: Vehicle
+    {
+        public int spareWheels = 1;
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Vehicle car1 = new Vehicle();
+            Corsa car2 = new Corsa();
+            Console.WriteLine(car2.wheels);
+
+            Console.ReadLine();
+        }
+    }
+}
